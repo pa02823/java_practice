ab0ad318bfd25a2e14ba9ca9683d9c2527a19ed3
pa02823 <princessasomba456@gmail.com>
2023-07-26 16:44:20 +0100

    java2 practice

---
+++ src/qa/Movable.java
@@ -0,0 +1,8 @@
+package qa;
+
+public interface Movable {
+    public Point getCurrentLocation();
+    public void move(double x, double y);
+
+
+}
---
+++ src/qa/Rectangle.java
@@ -0,0 +1,34 @@
+package qa;
+
+public class Rectangle extends Shape{
+    private double height, width;
+
+    public Rectangle(String name, String colour, double x, double y, double width, double height) {
+        super(name, colour, x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle [width=" + width + ", height=" + height + "]";
+    }
+
+    public boolean isSquare(){
+        return (width == height);
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public Point getCentrePoint() {
+        double centreX = getX() + width/2;
+        double centreY = getY() + height/2;
+        return new Point(centreX, centreY);
+    }
+
+
+}
